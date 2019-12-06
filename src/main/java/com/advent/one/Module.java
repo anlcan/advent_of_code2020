dc9c5755a8dad0959bc470d091622e20f97f20c1
anlcan <anil.canbaykal@finleap.com>
2019-12-06 23:53:04 +0100

    how that code worked in the first try?

--- src/main/java/com/advent/one/Module.java
+++ src/main/java/com/advent/one/Module.java
@@ -10,6 +10,7 @@ public class Module {
 
     private final BigDecimal mass;
     private static final BigDecimal TWO =  BigDecimal.valueOf(2);
+    private static final BigDecimal THREE =  BigDecimal.valueOf(3);
 
     public Module(int mass) {
         this.mass = new BigDecimal(mass);
@@ -24,21 +25,28 @@ public class Module {
         return requiredFuel(this.mass).intValue();
     }
 
+
     private static BigDecimal requiredFuel(BigDecimal mass){
-        return mass.compareTo(BigDecimal.ZERO) < 1? BigDecimal.ZERO : mass.divide(BigDecimal.valueOf(3), RoundingMode.DOWN).subtract(TWO);
+        return mass.divide(THREE, RoundingMode.DOWN).subtract(TWO);
     }
 
     public int requiredFullFuel (){
         return requiredFullFuel(this.mass).intValue();
     }
 
+    /**
+     * Fuel itself requires fuel just like a module - take its mass, divide by three, round down, and subtract 2.
+     * However, that fuel also requires fuel, and that fuel requires fuel, and so on. Any mass that would require negative fuel should
+     * instead be treated as if it requires zero fuel; the remaining mass, if any, is instead handled by wishing really hard,
+     * which has no mass and is outside the scope of this calculation.
+     */
     private static BigDecimal requiredFullFuel(BigDecimal mass){
+        BigDecimal fuel =  requiredFuel(mass);
 
-        if (mass.compareTo(BigDecimal.ZERO) > 0){
-            BigDecimal module =  requiredFuel(mass);
-            return module.add(requiredFullFuel(module));
-        } else {
-            return mass;
+        switch (fuel.compareTo(BigDecimal.ZERO)){
+            case 0: return fuel;
+            case 1: return fuel.add(requiredFullFuel(fuel));
+            default:  return BigDecimal.ZERO;
         }
     }
 
