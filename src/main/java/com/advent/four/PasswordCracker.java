a8881e995229617c3d3380678f2df6202e58a2cb
anlcan <anil.canbaykal@finleap.com>
2019-12-09 09:26:38 +0100

    partTwo

--- src/main/java/com/advent/four/PasswordCracker.java
+++ src/main/java/com/advent/four/PasswordCracker.java
@@ -19,23 +19,44 @@ public class PasswordCracker {
         this.end = end;
     }
 
-    public int possibilities() {
-        List<Integer> collect = IntStream.rangeClosed(start, end)
-                .filter(getDoubleDigit())
-                .filter(getIncreasing())
-                .boxed()
-                .collect(Collectors.toList());
-
-
-        return collect.size();
-    }
-
-    public static IntPredicate getDoubleDigit(){
+    /*
+    private final List<String> group = IntStream.rangeClosed(0, 9)
+            .mapToObj(i -> {
+                var str = String.valueOf(i);
+                return IntStream.rangeClosed(3,6).mapToObj(it -> str.repeat(it)).
+            })
+    */
+    public static IntPredicate getDoubleDigitTricky() {
         return (int x) -> {
             String s = String.valueOf(x);
-            for (int i = 0; i < s.length()-1; i++) {
-                if (s.charAt(i) == s.charAt(i+1))
+            for (int i = 0; i < s.length() - 1;) {
+                char c = s.charAt(i);
+                if (c == s.charAt(i + 1)) {
+                    if (i == s.length() - 2) {
+                        return true; // last two
+                    } else if (i < s.length() - 2 && c != s.charAt(i + 2)) {
+                        return true;
+                    } else {
+                        // found a group, skip until the end
+                        while (i < s.length() - 1 && c == s.charAt(i)) {
+                            i++;
+                        }
+                        continue;
+                    }
+                }
+                i++;
+            }
+            return false;
+        };
+    }
+
+    public static IntPredicate getDoubleDigit() {
+        return (int x) -> {
+            String s = String.valueOf(x);
+            for (int i = 0; i < s.length() - 1; i++) {
+                if (s.charAt(i) == s.charAt(i + 1)) {
                     return true;
+                }
             }
             return false;
         };
@@ -47,13 +68,36 @@ public class PasswordCracker {
             String s1 = String.valueOf(x);
             int[] s = s1.chars().toArray();
 
-            for (int i = 0; i < s1.length()-1; i++) {
+            for (int i = 0; i < s1.length() - 1; i++) {
 
-                if (s[i] > s[i+1])
+                if (s[i] > s[i + 1]) {
                     return false;
+                }
             }
             return true;
         };
+    }
+
+    public int possibilities() {
+        List<Integer> collect = IntStream.rangeClosed(start, end)
+                .filter(getDoubleDigit())
+                .filter(getIncreasing())
+                .boxed()
+                .collect(Collectors.toList());
+
+
+        return collect.size();
+    }
+
+    public int possibilitiesRefined() {
+        List<Integer> collect = IntStream.rangeClosed(start, end)
+                .filter(getDoubleDigitTricky())
+                .filter(getIncreasing())
+                .boxed()
+                .collect(Collectors.toList());
+
+        System.out.println(collect);
+        return collect.size();
     }
 
 }
--- src/test/java/com/advent2020/four/SecureContainerTest.java
+++ src/test/java/com/advent2020/four/SecureContainerTest.java
@@ -3,8 +3,7 @@ package com.advent2020.four;
 import com.advent.four.PasswordCracker;
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 /**
  * Created on 08.12.19.
@@ -18,6 +17,8 @@ public class SecureContainerTest {
         assertTrue(PasswordCracker.getDoubleDigit().test(1224));
         assertTrue(PasswordCracker.getDoubleDigit().test(1433));
 
+
+
         assertTrue(PasswordCracker.getIncreasing().test(1234));
         assertTrue(PasswordCracker.getIncreasing().test(1224));
         assertFalse(PasswordCracker.getIncreasing().test(1433));
@@ -25,8 +26,40 @@ public class SecureContainerTest {
     }
 
     @Test
+    public void trickyTest(){
+        assertTrue(PasswordCracker.getDoubleDigitTricky().test(1224));
+
+        //no longer meets the criteria (the repeated 44 is part of a
+        //larger group of 444)
+        assertFalse(PasswordCracker.getDoubleDigitTricky().test(123444));
+        assertFalse(PasswordCracker.getDoubleDigitTricky().test(12344441));
+        // meets the criteria (even though 1 is repeated more than twice,
+        //it still contains a double 22)
+        assertTrue(PasswordCracker.getDoubleDigitTricky().test(111122));
+        assertTrue(PasswordCracker.getDoubleDigitTricky().test(1111122));
+        assertTrue(PasswordCracker.getDoubleDigitTricky().test(11111122));
+        assertFalse(PasswordCracker.getDoubleDigitTricky().test(377777));
+        assertTrue(PasswordCracker.getDoubleDigitTricky().test(1553561222));
+        assertTrue(PasswordCracker.getDoubleDigitTricky().test(377899));
+
+        assertTrue(PasswordCracker.getDoubleDigitTricky().test(22101222));
+
+
+
+    }
+
+    @Test
     public void testPartOne() {
         var pc = new PasswordCracker(372304, 847060);
-        System.out.println(pc.possibilities());
+        System.out.println(pc.possibilities()); //475
+    }
+
+    @Test
+    public void testPartTwo() {
+
+        //trickyTest();
+        var pc = new PasswordCracker(372304, 847060);
+        //System.out.println(pc.possibilitiesRefined()); //297
+        assertEquals(297, pc.possibilitiesRefined()); //297
     }
 }
